180b09aa0fe5d9e365cc97114242f6b5b52c7200
Dhiraj <admin@admins-MBP.fios-router.home>
2019-12-18 00:37:20 -0500

    Fixing bugs

--- src/common/experience/experienceBlock.jsx
+++ src/common/experience/experienceBlock.jsx
@@ -1,32 +1,28 @@
-import React from 'react'
+import React from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Flip from "react-reveal/Flip";
 
 function ExperienceBlock(props) {
-    return (
-        <div className="col-sm-6 col-12 p-3">
-              <Card className={props.classes.card}>
-                <CardContent>
-                  <Typography variant="h6" component="h2"></Typography>
-                  <Flip left>
-                    <a
-                      href={props.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        src={props.src}
-                        alt={props.alt}
-                        className={props.classes.company}
-                      />
-                    </a>
-                  </Flip>
-                </CardContent>
-              </Card>
-            </div>
-    )
+  return (
+    <div className="col-sm-6 col-12 p-3">
+      <Card className={props.classes.card}>
+        <a href={props.href} target="_blank" rel="noopener noreferrer">
+          <CardContent>
+            <Typography variant="h6" component="h2"></Typography>
+            <Flip left>
+              <img
+                src={props.src}
+                alt={props.alt}
+                className={props.classes.company}
+              />
+            </Flip>
+          </CardContent>
+        </a>
+      </Card>
+    </div>
+  );
 }
 
-export default ExperienceBlock
+export default ExperienceBlock;
